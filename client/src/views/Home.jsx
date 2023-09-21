409979edbff0763694a402c2706d21fd2fad608d
Abdul Mohiz <FA20-BCS-001@cuiatd.edu.pk>
2023-09-21 23:43:36 +0500

    fix

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,6 +2,7 @@ import { Navbar, Sidebar } from "../components";
 import Content from "../components/Content";
 
 const Home = () => {
+  // home will have only 3 components navbar, sidebar and content
   return (
     <div className="flex flex-col gap-3">
       <Navbar />
